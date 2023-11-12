1b945e488727fb37b06d8e6455d18cc78fe08350
Sachin <sachinsharma2168@gmail.com>
2023-11-12 15:05:18 +0530

    Initial Commit

---
+++ PokeBall/PokeBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeBallApp.swift
+//  PokeBall
+//
+//  Created by Sachin Sharma on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
